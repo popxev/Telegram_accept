c1f8c87f9e4b72f7a0d89097727b24de016b959c
popxev <sandocraft10@gmail.com>
2025-03-30 03:45:49 +0100

    Fix webhook issue

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_m
 
 @app.route(f"/{TOKEN}", methods=["POST"])
 async def webhook():
-    update = Update.de_json(await request.json(), bot)
+    update = Update.de_json(await request.get_json(), bot)
     await application.process_update(update)
     return "OK", 200
 
