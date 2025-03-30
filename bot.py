bbebe619a5c5d12089bf9e53b8a9f321d3ee63a9
popxev <sandocraft10@gmail.com>
2025-03-30 18:19:14 +0100

    إصلاح الكود وإضافة route رئيسي

--- bot.py
+++ bot.py
@@ -8,17 +8,15 @@ from flask import Flask, request
 TOKEN = "7975587876:AAEPJnx7pt-qeqM41ijxg6dRU_wfzgEx1aA"
 bot = Bot(token=TOKEN)
 
-BANNED_WORDS = [
-    "كس", "كسمك", "كسك", "بوسة", "نيك", "نك", "حواك", "حوي", "نحويك", "نيكك", "زك", "زكك",
-    "قحب", "قحبة", "شرذيذ", "مضاجعة", "جماع", "ممارسة", "اغتصاب", "عاهرة", "دعارة", "مؤخرة",
-    "صدر", "ثدي", "فرج", "استمناء", "احتلام", "لواط", "سحاق", "شاذ", "شاذة", "زنا", "إباحية",
-    "سكسي", "sexy", "sex", "porn", "pussy", "dick", "ass", "boobs", "cock", "bitch", "slut"
-]
-
-BANNED_LINKS = ["xxx", "x", "xn", "porn", "www", "http", ".com", ".net", ".org", ".xyz"]
+BANNED_WORDS = ["كس", "كسمك", "بوسة", "نيك", "قحب", "إباحية", "sex", "porn"]
+BANNED_LINKS = ["xxx", "porn", "http", ".com", ".net"]
 
 app = Flask(__name__)
 application = Application.builder().token(TOKEN).build()
+
+@app.route('/')
+def home():
+    return "Bot is running!"
 
 @app.route(f'/{TOKEN}', methods=['POST'])
 def webhook():
@@ -32,18 +30,17 @@ async def set_webhook():
     await bot.set_webhook(url=webhook_url)
 
 async def start(update: Update, context):
-    await update.message.reply_text("مرحبا! أنا بوت Popxev Games. إذا كنت بحاجة للمساعدة، استخدم /help.")
+    await update.message.reply_text("مرحبا! أنا بوت Popxev Games. استخدم /help للمساعدة.")
 
 async def help(update: Update, context):
-    await update.message.reply_text("كيفية استخدام البوت: \n- لا تستخدم كلمات أو روابط ممنوعة. \n- إذا كنت بحاجة للتواصل، استخدم /contact.")
+    await update.message.reply_text("كيفية استخدام البوت: \n- لا تستخدم كلمات أو روابط ممنوعة. \n- استخدم /contact للتواصل.")
 
 async def contact(update: Update, context):
     await update.message.reply_text(
-        "للتواصل معنا: \n\n"
+        "للتواصل معنا: \n"
         "فيسبوك: [Popxev Games](https://www.facebook.com/share/1Dsxdcv7yN/) \n"
         "إنستجرام: [Popxev Games](https://www.instagram.com/popxev_games?igsh=anNwdzR5dXFwc2E4) \n"
-        "تيليجرام: [بوت Popxev](https://t.me/Popxevgamesgroup) \n"
-        "جروب تيليجرام: [Popxev Games Group](https://t.me/Popxevgamesgroup) \n"
+        "تيليجرام: [جروب Popxev](https://t.me/Popxevgamesgroup) \n"
         "ديسكورد: [Popxev Games Discord](https://discord.gg/tuRy8Qf7) \n"
     )
 
@@ -52,7 +49,7 @@ async def handle_message(update: Update, context):
     if any(word in text for word in BANNED_WORDS) or any(link in text for link in BANNED_LINKS):
         await update.message.delete()
         return
-    await update.message.reply_text("إذا كنت بحاجة إلى مساعدة، يمكنك استخدام الأمر /help.")
+    await update.message.reply_text("إذا كنت بحاجة إلى مساعدة، استخدم /help.")
 
 application.add_handler(CommandHandler("start", start))
 application.add_handler(CommandHandler("help", help))
